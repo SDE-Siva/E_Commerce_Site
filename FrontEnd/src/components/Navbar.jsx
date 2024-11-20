251d0128fcdabf1dfb994e3d750ddd515471dad2
SDE-Siva <journeyofsiva@gmail.com>
2024-11-20 20:13:32 +0530

    Feat:Navbar

---
+++ FrontEnd/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { assets } from "../assets/assets";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between py-5 font-medium ">
+      <img src={assets.logo} alt="" className="w-36" />
+
+      <ul className="hidden sm:flex gap-5 text-sm text-grey-700">
+        <NavLink to="/" className="flex flex-col items-center gap-1">
+          <p>hOME</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+
+        <NavLink to="/collections" className="flex flex-col items-center gap-1">
+          <p>COLLECTIONS</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+
+        <NavLink to="/about" className="flex flex-col items-center gap-1">
+          <p>ABOUT</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+
+        <NavLink to="/contact" className="flex flex-col items-center gap-1">
+          <p>CONTACT</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
