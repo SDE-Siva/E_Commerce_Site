72a90a425e97e6f22c7889f1601dd7c4b1f40b33
SDE-Siva <journeyofsiva@gmail.com>
2024-11-29 16:00:16 +0530

    Feat : Collections page updated

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -11,14 +11,16 @@ import PlaceOrder from "./Pages/PlaceOrder";
 import Orders from "./Pages/Orders";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import SearchBox from "./components/SearchBox";
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
       <Navbar />
+      <SearchBox />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/collection" element={<Collections />} />
+        <Route path="/collections" element={<Collections />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/product/:productId" element={<Product />} />
--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { assets } from "../assets/assets";
 import { Link, NavLink } from "react-router-dom";
+import { ShopContext } from "../Context/ShopContext";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
+
+  const { setShowSearch } = useContext(ShopContext);
+
   return (
     <div className="flex items-center justify-between py-5 font-medium ">
       <Link to="/">
@@ -33,7 +37,12 @@ const Navbar = () => {
       </ul>
 
       <div className="flex items-center gap-6">
-        <img className="w-5 cursor-pointer" src={assets.search_icon} alt="" />
+        <img
+          onClick={() => setShowSearch(true)}
+          className="w-5 cursor-pointer"
+          src={assets.search_icon}
+          alt=""
+        />
         <div className=" group relative ">
           <img
             className=" w-5 cursor-pointer "
